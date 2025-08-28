c3d4c4ef43ebae11eec36e0567a545bb8f43a4bf
chrisdeveloper2025 <chrisdeveloper2025@gmail.com>
2025-08-28 18:33:07 +0100

    280825

--- app.js
+++ app.js
@@ -2,13 +2,16 @@
 const navBtn = document.getElementById('nav-btn')
 const navbar = document.getElementById('navbar')
 const navClose = document.getElementById('nav-close')
+const mainContent = document.getElementById('mainContent')
 // show nav
 navBtn.addEventListener('click', () => {
   navbar.classList.add('showNav')
+  mainContent.classList.add('blur')
 })
 // close nav
 navClose.addEventListener('click', () => {
   navbar.classList.remove('showNav')
+  mainContent.classList.remove('blur')
 })
 //setup date
 const date = (document.getElementById('date').innerHTML =
